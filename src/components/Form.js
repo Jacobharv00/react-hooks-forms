1530ebc64b336f0b22e8ba0f95995671630ff0dd
jacob <jacobharv00@gmail.com>
2021-07-16 18:25:10 -0600

    testing eslint/prettier

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,21 @@
-import React from "react"
-
+import React from "react";
 
 function Form(props) {
   return (
     <form>
-      <input 
-        type="text" 
-        onChange={props.handleFirstNameChange} 
-        value={props.firstName} 
+      <input
+        type="text"
+        onChange={props.handleFirstNameChange}
+        value={props.firstName}
       />
-      <input 
-        type="text" 
-        onChange={props.handleLastNameChange} 
-        value={props.lastName} 
+      <input
+        type="text"
+        onChange={props.handleLastNameChange}
+        value={props.lastName}
       />
-
       <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
 
 export default Form;
